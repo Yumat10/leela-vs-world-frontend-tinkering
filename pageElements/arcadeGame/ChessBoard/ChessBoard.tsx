3af30fa0fd510ad341e13cbc37ed7fcf7397f16f
yuma <mr.yuma.tanaka@gmail.com>
2022-12-25 20:09:53 -0500

    Slowed down game play to 250ms

--- pageElements/arcadeGame/ChessBoard/ChessBoard.tsx
+++ pageElements/arcadeGame/ChessBoard/ChessBoard.tsx
@@ -80,7 +80,7 @@ export const ChessBoard: FC = () => {
       newCurrChessBoard[chessPieceNum as ChessPiece] = chosenChessPiece;
       setCurrChessBoard({ ...newCurrChessBoard });
       setTurnNum(turnNum + 1);
-    }, 100);
+    }, 250);
 
     return () => clearInterval(movePawnInterval);
   });
